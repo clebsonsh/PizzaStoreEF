1b7c9ee9b5cbdaeb7b01d58ba125a419b6009cff
Clebson Moura <clebson.sh@gmail.com>
2023-11-19 08:38:15 -0300

    Add endpoint to create a new pizza

--- Program.cs
+++ Program.cs
@@ -31,5 +31,11 @@ app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
 
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
 
 app.Run();
