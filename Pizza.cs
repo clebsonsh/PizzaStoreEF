974e39c6eb0dacb13fe1ce7a76a93b724ee82a39
Clebson Moura <clebson.sh@gmail.com>
2023-11-19 08:04:54 -0300

    Add PizzaDb class for database access

--- Pizza.cs
+++ Pizza.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 namespace PizzaStore.Models
 {
     public class Pizza
@@ -5,5 +6,11 @@ namespace PizzaStore.Models
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+    }
+
+    class PizzaDb : DbContext
+    {
+        public PizzaDb(DbContextOptions options) : base(options) { }
+        public DbSet<Pizza> Pizzas { get; set; } = null!;
     }
 }
